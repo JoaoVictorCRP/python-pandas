5d65ff763a8e3eac899a4d9f550610b721cd85d3
JoaoVictorCRP <joaovictorxl120@gmail.com>
2024-02-23 23:10:54 -0300

    mais funcoes

--- Introducao.py
+++ Introducao.py
@@ -1,19 +1,51 @@
 import pandas as pd
 
-# Lendo dataset (População mundial por país, de 1800 até 2100)
+# Lendo dataset 
 df = pd.read_csv('./gapminder-pop.csv')
 
 # Renomear Coluna
-df = df.rename(columns={'country':'País'})
+df = df.rename(columns={
+    'country':'País', 
+    'continent':'Continente',
+    'year':'Ano',
+    'lifeExp':'Expectativa de Vida',
+    'pop':'População', 
+    'gdpPercap':'PIB per Capita'
+})
 
 # Lendo as 5 primeiras linhas
-# print(df.head(10))
+# print(df.head())
+
+# Lendo as 5 últimas linhas
+# print(df.tail())
 
 # Total de linhas e colunas
-# print(df.shape[0])
+# print(df.shape)
 
 # Nome das colunas
 # print(df.columns)
 
 # Tipo de dado em cada coluna
-print(df.dtypes)
+# print(df.dtypes)
+
+# Informações estatísiticas da tabela
+# print(df.describe())
+
+# Filtrando a  tabela
+# print(df['País'].unique())
+
+# Aplicando filtros e colocando em uma variável
+# dados_brasil = df.loc[df['País'] == 'Brazil']
+# print(dados_brasil)
+
+# Agrupando dados ( Group By)
+# agrupado_continentes = df.groupby('Continente')['País'].nunique()
+# print(agrupado_continentes)
+
+# Expectativa de vida média, agrupada por ano
+# expec_vida_media_por_ano = df.groupby('Ano')['Expectativa de Vida'].mean()
+# print(expec_vida_media_por_ano)
+
+# PIB Global (PIB somado)
+pib_global = df['PIB per Capita'].sum()
+print(pib_global)
